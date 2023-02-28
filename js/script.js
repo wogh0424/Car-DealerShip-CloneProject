f7ed58d6812e6610793dd2a7f933b3426be2bed7
Jaeho Kim <wogh0328@naver.com>
2023-02-28 23:08:49 +0900

    first commit

--- js/script.js
+++ js/script.js
@@ -14,26 +14,29 @@ menu.onclick = () =>{
     document.querySelector('.login-form-container').classList.remove('active');
   }
 
+  menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+  }
+  
+  document.querySelector('#login-btn').onclick = () =>{
+    document.querySelector('.login-form-container').classList.toggle('active');
+  }
+  
+  document.querySelector('#close-login-form').onclick = () =>{
+    document.querySelector('.login-form-container').classList.remove('active');
+  }
   window.onscroll = () =>{
 
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active')
-    }else{
-        document.querySelector('.header').classList.remove('active')
-    }
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
- 
-  }
-  window.onload = () =>{
+  
     if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
+      document.querySelector('.header').classList.add('active');
     }else{
-        document.querySelector('.header').classList.remove('active');
+      document.querySelector('.header').classList.remove('active');
     };
-
-    
-
+  
   };
 
   document.querySelector('.home').onmousemove = (e) =>{
@@ -48,4 +51,5 @@ menu.onclick = () =>{
       elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
   
     });
+  
   };
